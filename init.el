ce3e2a536a6ad41580dee6434f5c7a7eb2c82b06
artsi0m <karakin2000@gmail.com>
2023-04-15 00:46:50 +0300

    Use minsk theme only in GUI.

---
+++ init.el
@@ -0,0 +1,23 @@
+;; artsi0m's emacs config
+;; Windows edition
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/" ) t)
+
+(package-initialize)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("7e300d88af7750886190c744f63d2d66580bb2cbb8e371a3cb5109afe3017a5a" default))
+ '(package-selected-packages '(org-drill minsk-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(if window-system (load-theme 'minsk t))
