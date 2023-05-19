3e0a024700c2f30269f1335d6a8dbe41be549953
artsi0m <karakin2000@gmail.com>
2023-05-19 10:20:23 +0300

    A lot of changes:
    I started using org-pomodoro to manage time spent on tasks and breaks.
    I configured emacs to use C locale or else it will write
    timestamps to file in CP1251 encoding.
    
    Recently i also started to use calfw. I installed calfw-icalc
    to import BSUIR schedule and also added file with all of my student
    TODO's into org-agenda.

--- init.el
+++ init.el
@@ -1,6 +1,9 @@
 ;; artsi0m's emacs config
 ;; Windows edition
 
+
+(setq system-time-locale "C") ;; For dealing with org-pomodoro
+
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/" ) t)
 
@@ -12,12 +15,56 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("7e300d88af7750886190c744f63d2d66580bb2cbb8e371a3cb5109afe3017a5a" default))
- '(package-selected-packages '(org-drill minsk-theme)))
+ '(org-display-custom-times t)
+ '(org-pomodoro-audio-player 'sound-wav)
+ '(org-time-stamp-custom-formats '("<%y-%m-%d>" . "<%y-%m-%d %H:%M>"))
+ '(package-selected-packages
+   '(calfw-ical calfw-org calfw sound-wav org-pomodoro org-drill minsk-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Iosevka" :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
 
 (if window-system (load-theme 'minsk t))
+
+;; メモフォーマットをorgに
+(require 'org)
+;; latex formulas
+(setq org-format-latex-options (plist-put org-format-latex-options :scale 1.7))
+;; howm
+(add-hook 'org-mode-hook 'howm-mode)
+(add-to-list 'auto-mode-alist '("\\.howm$" . org-mode))
+(add-to-list 'auto-mode-alist '("\\.txt$" . org-mode))
+(setq howm-view-title-header "*") ;; ← howm のロードより前に書くこと
+(setq org-startup-folded nil)
+(setq howm-home-directory "~/Documents/howm/")
+(setq howm-directory "~/Documents/howm/")
+(require 'howm)
+
+;; configure sources for org-agenda
+(setq org-agenda-files (list "~/Documents/Bsuir/4sem.org"
+			     ))
+
+;; TODO 
+;; configure org-gcal to sync with google-calendar which contains
+;; bsuir schedule
+
+
+;; calfw
+(require 'calfw)
+(require 'calfw-org)
+(require 'calfw-ical)
+
+(defun my-open-calendar ()
+  (interactive)
+  (cfw:open-calendar-buffer
+   :contents-sources
+   (list
+    (cfw:org-create-source)
+    (cfw:ical-create-source "gcal"
+			    "https://calendar.google.com/calendar/ical/m6a3jqdi6imnv97h39v9r528k4%40group.calendar.google.com/public/basic.ics"
+			    "Purple"
+			    )
+    )))
