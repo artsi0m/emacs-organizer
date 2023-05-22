9d0bbc9190f6578e0748404da5b140d846be8e94
artsi0m <karakin2000@gmail.com>
2023-05-22 10:20:42 +0300

    Split config

--- init.el
+++ init.el
@@ -43,28 +43,5 @@
 (setq howm-directory "~/Documents/howm/")
 (require 'howm)
 
-;; configure sources for org-agenda
-(setq org-agenda-files (list "~/Documents/Bsuir/4sem.org"
-			     ))
-
-;; TODO 
-;; configure org-gcal to sync with google-calendar which contains
-;; bsuir schedule
-
-
-;; calfw
-(require 'calfw)
-(require 'calfw-org)
-(require 'calfw-ical)
-
-(defun my-open-calendar ()
-  (interactive)
-  (cfw:open-calendar-buffer
-   :contents-sources
-   (list
-    (cfw:org-create-source)
-    (cfw:ical-create-source "gcal"
-			    "https://calendar.google.com/calendar/ical/m6a3jqdi6imnv97h39v9r528k4%40group.calendar.google.com/public/basic.ics"
-			    "Purple"
-			    )
-    )))
+;; Load calendar initialization
+(load "~/.emacs.d/init-calendar.el")
