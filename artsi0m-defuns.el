ff3ebe881445d8fd91232a6239cf7d9604a0fa8e
artsi0m <karakin2000@gmail.com>
2023-09-26 13:19:40 +0300

    simpler expand-file-names-in-dir

--- artsi0m-defuns.el
+++ artsi0m-defuns.el
@@ -1,5 +1,3 @@
-(defun expand-file-names-in-dir (directory)
-  "Return list of absolute filenames of files in directory ommitting dotfiles"
-  (let ((default-directory directory))
-    (mapcar #'expand-file-name
-	    (directory-files directory t directory-files-no-dot-files-regexp))))
+(defun expand-file-names-in-dir (dir)
+	 "Return list of absolute filenames in directory ommiting dotfiles"
+	 (directory-files dir t directory-files-no-dot-files-regexp))
