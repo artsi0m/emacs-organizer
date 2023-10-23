ff7dd8adcb918c664d08586e03de6a678f76aa86
artsi0m <karkin2000@gmail.com>
2023-10-23 12:36:57 +0300

    I decided to put org-drill files in howm

--- artsi0m-defuns.el
+++ artsi0m-defuns.el
@@ -5,3 +5,7 @@
 (defun my-howm-elfeed-file-names ()
 	   "Return list of absolute filenames of org-elfeed files in howm"
 	 (mapcar #'car (howm-grep "\:elfeed\:" (howm-files-in-directory howm-directory))))
+
+ (defun my-org-drill-file-names-in-howm ()
+	   "Return list of absolute filenames of org-drill files in howm"
+	 (mapcar #'car (howm-grep "\:drill\:" (howm-files-in-directory howm-directory))))
