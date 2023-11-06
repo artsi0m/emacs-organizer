540dc9b751139c1c651775f7a9e9e97f3e3a4ba8
artsi0m <karakin2000@gmail.com>
2023-11-07 00:07:20 +0300

    delete-dusp for lists with files in howm

--- artsi0m-defuns.el
+++ artsi0m-defuns.el
@@ -3,13 +3,18 @@
 	 (directory-files dir t directory-files-no-dot-files-regexp))
 
 (defun my-elfeed-file-names-in-howm ()
-	   "Return list of absolute filenames of org-elfeed files in howm"
-	 (mapcar #'car (howm-grep "\:elfeed\:" (howm-files-in-directory howm-directory))))
+  "Return list of absolute filenames of org-elfeed files in howm"
+  (delete-dups
+   (mapcar #'car (howm-grep "\:elfeed\:"
+		      (howm-files-in-directory howm-directory)))))
 
 (defun my-org-drill-file-names-in-howm ()
-	   "Return list of absolute filenames of org-drill files in howm"
-	   (mapcar #'car (howm-grep "\:drill\:" (howm-files-in-directory howm-directory))))
+  "Return list of absolute filenames of org-drill files in howm"
+  (delete-dups
+   (mapcar #'car (howm-grep "\:drill\:"
+			    (howm-files-in-directory howm-directory)))))
 
 (defun my-org-agenda-file-names-in-howm()
-	 "Return list of absoulute filenames of files with :agenda: tag in howm"
-	 (mapcar #'car (howm-grep "\:agenda\:" (howm-files-in-directory howm-directory))))
+  "Return list of absoulute filenames of files with :agenda: tag in howm"
+  (delete-dups
+   (mapcar #'car (howm-grep "\:agenda\:" (howm-files-in-directory howm-directory)))))
