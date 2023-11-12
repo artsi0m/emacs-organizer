86086a0addc834d5241c3b3f36ec22df7309e53b
artsi0m <karakin2000@gmail.com>
2023-11-12 18:14:54 +0300

    Config for mentor

---
+++ torrenting.el
@@ -0,0 +1,5 @@
+(use-package mentor
+  :init (setq mentor-rtorrent-client-version "0.9.6")
+  (setq mentor-rtorrent-library-version "0.13.6")
+  (setq mentor-rtorrent-name "rtorrent")
+  :custom  (mentor-rtorrent-external-rpc "/home/artsi0m/.rtorrent-rpc.socket"))
