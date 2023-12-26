632d890beda44791a178eace7d7c6a1527e9a04e
artsi0m <karakin2000@gmail.com>
2023-12-27 01:27:16 +0300

    Started using GNUS with gmail. org-babel-C-compiler set to clang

--- init.el
+++ init.el
@@ -105,13 +105,28 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes '(modus-operandi))
  '(custom-safe-themes
-   '("a197d6d98f7a01991275578aa0a1311363d4662f0ecfa8e15779ce63e0f76baa" "7e300d88af7750886190c744f63d2d66580bb2cbb8e371a3cb5109afe3017a5a" default))
+   '("fc608d4c9f476ad1da7f07f7d19cc392ec0fb61f77f7236f2b6b42ae95801a62" "a197d6d98f7a01991275578aa0a1311363d4662f0ecfa8e15779ce63e0f76baa" "7e300d88af7750886190c744f63d2d66580bb2cbb8e371a3cb5109afe3017a5a" default))
  '(default-input-method "cyrillic-colemak")
+ '(gnus-select-method
+   '(nnimap "imap.gmail.com"
+	    (nnmail-expiry-target "nnimap+gmail:[Gmail]/Корзина")
+	    (nnimap-stream ssl)
+	    (gnus-search-engine gnus-search-imap)))
+ '(message-send-mail-function 'smtpmail-send-it)
+ '(message-sendmail-envelope-from nil)
+ '(org-babel-C-compiler "clang")
  '(org-pomodoro-audio-player 'sound-wav)
  '(package-selected-packages
    '(pyvenv org-timeblock mentor racket-mode consult-eglot consult elfeed-protocol modus-operandi calfw org-drill-table powershell auctex vc-use-package elfeed-org elfeed org-roam-ql vertico eglot org org-ql esup transpose-frame fb2-reader howm sound-wav org-pomodoro org-drill minsk-theme))
+ '(smtpmail-default-smtp-server "smtp.gmail.com")
+ '(smtpmail-servers-requiring-authorization "smtp.gmail.com")
+ '(smtpmail-smtp-server "smtp.gmail.com")
+ '(smtpmail-smtp-service 587)
+ '(smtpmail-stream-type nil)
  '(speedbar-default-position 'left)
- '(tab-bar-mode t))
+ '(tab-bar-mode t)
+ '(user-full-name "Корякин Артём")
+ '(user-mail-address "karakin2000@gmail.com"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
