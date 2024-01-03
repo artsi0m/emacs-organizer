ab790da07e6c2b4d030b2e36d12c392b0f64cedb
artsi0m <karakin2000@gmail.com>
2024-01-03 04:59:49 +0300

    (my-howm-subsubdirs)

--- artsi0m-defuns.el
+++ artsi0m-defuns.el
@@ -14,8 +14,29 @@
    (mapcar #'car (howm-grep "\:drill\:"
 			    (howm-files-in-directory howm-directory)))))
 
-(defun my-org-agenda-file-names-in-howm()
+(defun my-org-agenda-file-names-in-howm ()
   "Return list of absoulute filenames of files with :agenda: tag in howm"
   (delete-dups
    (mapcar #'car (howm-grep "\:agenda\:"
 			    (howm-files-in-directory howm-directory)))))
+
+
+(defun my-howm-subsubdir-exclude (dir)
+  (if (string-match-p my-howm-subsubdir-regexp dir)
+      (unless
+	  (string-match-p my-howm-excluded-subdirs-regexp dir) dir)
+    nil))
+
+
+;; (defun my-howm-subsubdirs ()
+;;     (seq-filter #'my-howm-subsubdir-exclude
+;; 	    (seq-filter #'file-directory-p
+;; 	    (directory-files-recursively
+;; 	     howm-directory directory-files-no-dot-files-regexp t))))
+
+(defun my-howm-subsubdirs ()
+  (thread-last
+    (directory-files-recursively
+     howm-directory directory-files-no-dot-files-regexp t)
+    (seq-filter #'file-directory-p)
+    (seq-filter #'my-howm-subsubdir-exclude)))
